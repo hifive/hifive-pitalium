523d5a2d1150f45d010886a2c69953ccdbc732db
Marie Sakai <sakai.marie.6b4@jp.nssol.nssmc.com>
2015-08-11 15:43:58 +0900

    #25 PtlInternetExplorer8WebElementで座標を-2していた処理を削除

--- pitalium/src/main/java/com/htmlhifive/pitalium/core/selenium/PtlInternetExplorer8WebElement.java
+++ pitalium/src/main/java/com/htmlhifive/pitalium/core/selenium/PtlInternetExplorer8WebElement.java
@@ -19,15 +19,4 @@ package com.htmlhifive.pitalium.core.selenium;
  * Internet Explorer 8で利用する{@link org.openqa.selenium.WebElement}
  */
 class PtlInternetExplorer8WebElement extends PtlInternetExplorerWebElement {
-
-	@Override
-	public WebElementRect getRect() {
-		// IE7と8はbody以外座標を-2する
-		WebElementRect rect = super.getRect();
-		if ("body".equals(getTagName())) {
-			return rect;
-		}
-
-		return new WebElementRect(rect.getLeft() - 2d, rect.getTop() - 2d, rect.getWidth(), rect.getHeight());
-	}
 }
