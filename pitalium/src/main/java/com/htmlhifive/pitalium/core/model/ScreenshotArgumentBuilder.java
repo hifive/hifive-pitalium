b08ca948584cd7ce007b5cf8a022a2f3afc250e2
nakatani <nakatani.taizo.38x@jp.nssol.nssmc.com>
2015-09-15 17:11:20 +0900

    ScreenshotArgumentを作成するビルダークラスの骨格追加

---
+++ pitalium/src/main/java/com/htmlhifive/pitalium/core/model/ScreenshotArgumentBuilder.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (C) 2015 NS Solutions Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.htmlhifive.pitalium.core.model;
+
+/**
+ * TODO JavaDoc
+ *
+ * @author nakatani
+ */
+public class ScreenshotArgumentBuilder {
+
+	protected ScreenshotArgumentBuilder() {
+		// TODO
+	}
+
+	protected ScreenshotArgumentBuilder(String screenshotId) {
+		// TODO
+	}
+
+	public ScreenshotArgument build() {
+		throw new UnsupportedOperationException("TODO");
+	}
+
+	public ScreenshotArgumentBuilder screenshotId(String screenshotId) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addNewTarget() {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addNewTarget(SelectorType type, String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addNewTargetById(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addNewTargetByClassName(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addNewTargetByCssSelector(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addNewTargetByLinkText(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addNewTargetByName(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addNewTargetByPartialLinkText(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addNewTargetByTagName(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addNewTargetByXPath(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addNewTarget(double x, double y, double width, double height) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addExclude(SelectorType type, String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addExcludeById(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addExcludeByClassName(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addExcludeByCssSelector(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addExcludeByLinkText(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addExcludeByName(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addExcludeByPartialLinkText(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addExcludeByTagName(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addExcludeByXPath(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addExclude(double x, double y, double width, double height) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder moveTarget(boolean moveTarget) {
+		// TODO
+		return this;
+	}
+
+	//<editor-fold desc="HiddenElementSelector">
+
+	public ScreenshotArgumentBuilder addHiddenElementSelector(SelectorType type, String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addHiddenElementsById(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addHiddenElementsByClassName(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addHiddenElementsByCssSelector(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addHiddenElementsByLinkText(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addHiddenElementsByName(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addHiddenElementsByPartialLinkText(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addHiddenElementsByTagName(String value) {
+		// TODO
+		return this;
+	}
+
+	public ScreenshotArgumentBuilder addHiddenElementsByXPath(String value) {
+		// TODO
+		return this;
+	}
+
+	//</editor-fold>
+
+}
