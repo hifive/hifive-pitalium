c7e1eeb6ee4b0c4655e6188d1c7745e11c8332cd
nakatani <nakatani.taizo.38x@jp.nssol.nssmc.com>
2015-09-15 17:10:15 +0900

    スクリーンショット撮影の引数をまとめたScreenshotArgument追加

---
+++ pitalium/src/main/java/com/htmlhifive/pitalium/core/model/ScreenshotArgument.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2015 NS Solutions Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.htmlhifive.pitalium.core.model;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.google.common.base.Strings;
+
+/**
+ * TODO JavaDoc
+ * 
+ * @author nakatani
+ */
+public class ScreenshotArgument {
+
+	private static <T> List<T> toUnmodifiableList(List<T> list) {
+		if (list == null || list.isEmpty()) {
+			return Collections.emptyList();
+		} else {
+			return Collections.unmodifiableList(list);
+		}
+	}
+
+	public static ScreenshotArgumentBuilder builder() {
+		return new ScreenshotArgumentBuilder();
+	}
+
+	public static ScreenshotArgumentBuilder builder(String screenshotId) {
+		return new ScreenshotArgumentBuilder(screenshotId);
+	}
+
+	private final String screenshotId;
+
+	private final List<CompareTarget> targets;
+
+	private final List<DomSelector> hiddenElementSelectors;
+
+	protected ScreenshotArgument(String screenshotId, List<CompareTarget> targets,
+			List<DomSelector> hiddenElementSelectors) {
+		if (Strings.isNullOrEmpty(screenshotId)) {
+			throw new NullPointerException("screenshotId");
+		}
+
+		this.screenshotId = screenshotId;
+		this.targets = toUnmodifiableList(targets);
+		this.hiddenElementSelectors = toUnmodifiableList(hiddenElementSelectors);
+	}
+
+	public String getScreenshotId() {
+		return screenshotId;
+	}
+
+	public List<CompareTarget> getTargets() {
+		return targets;
+	}
+
+	public List<DomSelector> getHiddenElementSelectors() {
+		return hiddenElementSelectors;
+	}
+
+	public ScreenshotArgument withScreenshotId(String screenshotId) {
+		return new ScreenshotArgument(screenshotId, targets, hiddenElementSelectors);
+	}
+
+}
