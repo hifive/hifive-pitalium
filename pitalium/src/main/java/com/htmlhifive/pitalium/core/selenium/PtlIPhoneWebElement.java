10af930d596d41b88e419459c6cd61ffe1c97f5e
Marie Sakai <sakai.marie.6b4@jp.nssol.nssmc.com>
2016-02-09 18:12:44 +0900

    #11 iPhoneでスクロール量の調整がされていなかったため、調整処理を追加

--- pitalium/src/main/java/com/htmlhifive/pitalium/core/selenium/PtlIPhoneWebElement.java
+++ pitalium/src/main/java/com/htmlhifive/pitalium/core/selenium/PtlIPhoneWebElement.java
@@ -15,8 +15,23 @@
  */
 package com.htmlhifive.pitalium.core.selenium;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * iPhoneで利用する{@link org.openqa.selenium.WebElement}
  */
 class PtlIPhoneWebElement extends PtlWebElement {
+	private static final Logger LOG = LoggerFactory.getLogger(PtlIPhoneWebElement.class);
+
+	@Override
+	public int scrollNext() throws InterruptedException {
+		long initialScrollTop = (int) Math.round(getCurrentScrollTop());
+		long clientHeight = getClientHeight();
+		LOG.debug("[Scroll element] next to ({}, {}) ({})", 0, initialScrollTop + clientHeight, this);
+		// 外枠の影を切り取るのでスクロール量を減らす
+		scrollTo(0, initialScrollTop + clientHeight - 3);
+		long currentScrollTop = (int) Math.round(getCurrentScrollTop());
+		return (int) (currentScrollTop - initialScrollTop);
+	}
 }
