300df5a0c7a3892da95884d5cb9fb3c45d53ed5d
tkashi <kashimua.takuya.ca2@jp.nssol.nssmc.com>
2017-04-13 18:42:56 +0900

    #108,#121
    座標要素を丸めて出力するように修正。
    targetは指定した要素に収まる最大の短径になるようにし、excludeは指定した要素が収まる最小の短径となるように、座標を丸める。

--- pitalium/src/main/java/com/htmlhifive/pitalium/core/rules/AssertionView.java
+++ pitalium/src/main/java/com/htmlhifive/pitalium/core/rules/AssertionView.java
@@ -191,7 +191,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * {@link org.openqa.selenium.Capabilities}に応じた{@link PtlWebDriver}を作成して返します。
-	 * 
+	 *
 	 * @param cap ブラウザスペック情報
 	 * @return ブラウザに対応するWebDriver
 	 */
@@ -223,7 +223,7 @@ public class AssertionView extends TestWatcher {
 	 * 指定の条件でスクリーンショットを撮影します。テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の時は
 	 * 正解状態としてスクリーンショットの画像と座標を保存します。 テスト実行モードが {@link com.htmlhifive.pitalium.core.config.ExecMode#RUN_TEST}の時は、
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。
-	 * 
+	 *
 	 * @param screenshotId スクリーンショットを識別するID
 	 */
 	public void assertView(String screenshotId) {
@@ -234,7 +234,7 @@ public class AssertionView extends TestWatcher {
 	 * 指定の条件でスクリーンショットを撮影します。テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の時は
 	 * 正解状態としてスクリーンショットの画像と座標を保存します。 テスト実行モードが {@link com.htmlhifive.pitalium.core.config.ExecMode#RUN_TEST}の時は、
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。
-	 * 
+	 *
 	 * @param message {@link AssertionError}を識別する文字列
 	 * @param screenshotId スクリーンショットを識別するID
 	 */
@@ -247,7 +247,7 @@ public class AssertionView extends TestWatcher {
 	 * 指定の条件でスクリーンショットを撮影します。テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の時は
 	 * 正解状態としてスクリーンショットの画像と座標を保存します。 テスト実行モードが {@link com.htmlhifive.pitalium.core.config.ExecMode#RUN_TEST}の時は、
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。
-	 * 
+	 *
 	 * @param screenshotId スクリーンショットを識別するID
 	 * @param compareTargets スクリーンショットの撮影、比較条件
 	 */
@@ -259,7 +259,7 @@ public class AssertionView extends TestWatcher {
 	 * 指定の条件でスクリーンショットを撮影します。テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の時は
 	 * 正解状態としてスクリーンショットの画像と座標を保存します。 テスト実行モードが {@link com.htmlhifive.pitalium.core.config.ExecMode#RUN_TEST}の時は、
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。
-	 * 
+	 *
 	 * @param screenshotId スクリーンショットを識別するID
 	 * @param compareTargets スクリーンショットの撮影、比較条件
 	 */
@@ -271,7 +271,7 @@ public class AssertionView extends TestWatcher {
 	 * 指定の条件でスクリーンショットを撮影します。テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の時は
 	 * 正解状態としてスクリーンショットの画像と座標を保存します。 テスト実行モードが {@link com.htmlhifive.pitalium.core.config.ExecMode#RUN_TEST}の時は、
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。
-	 * 
+	 *
 	 * @param message {@link AssertionError}を識別する文字列
 	 * @param screenshotId スクリーンショットを識別するID
 	 * @param compareTargets スクリーンショットの撮影、比較条件
@@ -284,7 +284,7 @@ public class AssertionView extends TestWatcher {
 	 * 指定の条件でスクリーンショットを撮影します。テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の時は
 	 * 正解状態としてスクリーンショットの画像と座標を保存します。 テスト実行モードが {@link com.htmlhifive.pitalium.core.config.ExecMode#RUN_TEST}の時は、
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。
-	 * 
+	 *
 	 * @param screenshotId スクリーンショットを識別するID
 	 * @param compareTargets スクリーンショットの撮影、比較条件
 	 * @param hiddenElementsSelectors スクリーンショット撮影時に非表示にするDOMを表すセレクターのコレクション
@@ -297,7 +297,7 @@ public class AssertionView extends TestWatcher {
 	 * 指定の条件でスクリーンショットを撮影します。テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の時は
 	 * 正解状態としてスクリーンショットの画像と座標を保存します。 テスト実行モードが {@link com.htmlhifive.pitalium.core.config.ExecMode#RUN_TEST}の時は、
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。
-	 * 
+	 *
 	 * @param screenshotId スクリーンショットを識別するID
 	 * @param compareTargets スクリーンショットの撮影、比較条件
 	 * @param hiddenElementsSelectors スクリーンショット撮影時に非表示にするDOMを表すセレクターのコレクション
@@ -311,7 +311,7 @@ public class AssertionView extends TestWatcher {
 	 * 指定の条件でスクリーンショットを撮影します。テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の時は
 	 * 正解状態としてスクリーンショットの画像と座標を保存します。 テスト実行モードが {@link com.htmlhifive.pitalium.core.config.ExecMode#RUN_TEST}の時は、
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。
-	 * 
+	 *
 	 * @param arg スクリーンショットを撮影するための条件
 	 */
 	public void assertView(ScreenshotArgument arg) {
@@ -322,7 +322,7 @@ public class AssertionView extends TestWatcher {
 	 * 指定の条件でスクリーンショットを撮影します。テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の時は
 	 * 正解状態としてスクリーンショットの画像と座標を保存します。 テスト実行モードが {@link com.htmlhifive.pitalium.core.config.ExecMode#RUN_TEST}の時は、
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。
-	 * 
+	 *
 	 * @param message {@link AssertionError}を識別する文字列
 	 * @param arg スクリーンショットを撮影するための条件
 	 */
@@ -334,7 +334,7 @@ public class AssertionView extends TestWatcher {
 	 * 指定の条件でスクリーンショットを撮影します。テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の時は
 	 * 正解状態としてスクリーンショットの画像と座標を保存します。 テスト実行モードが {@link com.htmlhifive.pitalium.core.config.ExecMode#RUN_TEST}の時は、
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。
-	 * 
+	 *
 	 * @param message {@link AssertionError}を識別する文字列
 	 * @param screenshotId スクリーンショットを識別するID
 	 * @param compareTargets スクリーンショットの撮影、比較条件
@@ -430,7 +430,7 @@ public class AssertionView extends TestWatcher {
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。<br />
 	 * <br />
 	 * {@link #assertView(ScreenshotArgument)}との違いは{@code RUN_TEST}時に比較が失敗してもテストの実行を止めず、テストを最後まで実行します。
-	 * 
+	 *
 	 * @param arg スクリーンショットを撮影するための条件
 	 */
 	public void verifyView(ScreenshotArgument arg) {
@@ -449,7 +449,7 @@ public class AssertionView extends TestWatcher {
 	 * {@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}で撮影した状態と比較します。<br />
 	 * <br />
 	 * {@link #assertView(ScreenshotArgument)}との違いは{@code RUN_TEST}時に比較が失敗してもテストの実行を止めず、テストを最後まで実行します。
-	 * 
+	 *
 	 * @param message {@link AssertionError}を識別する文字列
 	 * @param arg スクリーンショットを撮影するための条件
 	 */
@@ -465,7 +465,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * スクリーンショットの撮影結果をチェックします。
-	 * 
+	 *
 	 * @param targetResults 実行結果
 	 * @param compareTargets 指定した{@link CompareTarget}
 	 * @return バリデーション結果
@@ -477,7 +477,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * CompareTargetで指定したセレクタに対応する要素が大きさを持っているかチェックします。
-	 * 
+	 *
 	 * @param targetResults 指定した{@link CompareTarget}
 	 * @return 大きさを持たない要素のセレクタのリスト
 	 */
@@ -497,7 +497,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * CompareTargetで指定したセレクタに対応する要素が存在したかチェックします。
-	 * 
+	 *
 	 * @param targetResults 実行結果
 	 * @param compareTargets 指定した{@link CompareTarget}
 	 * @return 存在しなかった要素のセレクタのリスト
@@ -530,7 +530,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * 1つのスクリーンショットIDに対応する実行結果を保存します。
-	 * 
+	 *
 	 * @param screenshotId スクリーンショットID
 	 * @param targetResults 実行結果のリスト
 	 */
@@ -539,7 +539,8 @@ public class AssertionView extends TestWatcher {
 				capabilities);
 		List<TargetResult> processes = new ArrayList<TargetResult>(targetResults.size());
 		for (TargetResult target : targetResults) {
-			processes.add(new TargetResult(target.getTarget(), target.getExcludes(), target.getImage()));
+			processes.add(new TargetResult(toTargetForJson(target.getTarget()), toExcludesForJson(target.getExcludes()),
+					target.getImage()));
 		}
 
 		TestResultManager.getInstance().getPersister().saveTargetResults(currentMetadata, processes);
@@ -547,7 +548,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * スクリーンショットを撮影し、結果画像を保存します。
-	 * 
+	 *
 	 * @param screenshotId スクリーンショットを識別するID
 	 * @param compareTargets スクリーンショットの撮影、比較条件
 	 * @param hiddenElementsSelectors スクリーンショット撮影時に非表示にするDOMを表すセレクターのコレクション
@@ -594,7 +595,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * Expectedモード時のScreenshotResultを作成します。
-	 * 
+	 *
 	 * @param screenshotId スクリーンショットID
 	 * @param targetResults 実行結果
 	 * @param validateResult バリデーション結果
@@ -604,8 +605,9 @@ public class AssertionView extends TestWatcher {
 			ValidateResult validateResult) {
 		List<TargetResult> processes = new ArrayList<TargetResult>(targetResults.size());
 		for (TargetResult target : targetResults) {
-			processes.add(new TargetResult(null, target.getTarget(), target.getExcludes(), target.isMoveTarget(),
-					target.getHiddenElementSelectors()));
+			processes.add(
+					new TargetResult(null, toTargetForJson(target.getTarget()), toExcludesForJson(target.getExcludes()),
+							target.isMoveTarget(), target.getHiddenElementSelectors()));
 		}
 
 		for (DomSelector selector : validateResult.noElementSelectors) {
@@ -620,7 +622,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * Actualモード時のScreenshotResultを作成します。
-	 * 
+	 *
 	 * @param screenshotId スクリーンショットID
 	 * @param expectedId 期待結果ID
 	 * @param currents 今回の実行結果
@@ -637,8 +639,9 @@ public class AssertionView extends TestWatcher {
 			// Target element's area was nothing
 			IndexDomSelector selector = current.getTarget().getSelector();
 			if (selector != null && validateResult.noAreaElementSelectors.contains(selector)) {
-				processes.add(new TargetResult(ExecResult.FAILURE, current.getTarget(), current.getExcludes(),
-						current.isMoveTarget(), current.getHiddenElementSelectors()));
+				processes.add(new TargetResult(ExecResult.FAILURE, toTargetForJson(current.getTarget()),
+						toExcludesForJson(current.getExcludes()), current.isMoveTarget(),
+						current.getHiddenElementSelectors()));
 				LOG.debug("[Comparison skipped] ({})", current.getTarget());
 				continue;
 			}
@@ -653,7 +656,8 @@ public class AssertionView extends TestWatcher {
 				});
 			} catch (NoSuchElementException e) {
 				LOG.error("[Comparison failed] No element found for target ({}).", current.getTarget());
-				processes.add(new TargetResult(null, current.getTarget(), current.getExcludes(), current.isMoveTarget(),
+				processes.add(new TargetResult(null, toTargetForJson(current.getTarget()),
+						toExcludesForJson(current.getExcludes()), current.isMoveTarget(),
 						current.getHiddenElementSelectors()));
 				assertFail = true;
 
@@ -674,8 +678,8 @@ public class AssertionView extends TestWatcher {
 			}
 
 			processes.add(new TargetResult(compareResult.isSucceeded() ? ExecResult.SUCCESS : ExecResult.FAILURE,
-					current.getTarget(), current.getExcludes(), current.isMoveTarget(),
-					current.getHiddenElementSelectors()));
+					toTargetForJson(current.getTarget()), toExcludesForJson(current.getExcludes()),
+					current.isMoveTarget(), current.getHiddenElementSelectors()));
 
 			// 比較でFailだった場合、差分の画像を作成
 			if (compareResult.isFailed()) {
@@ -708,7 +712,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * スクリーンショット比較の前準備として、除外領域をマスクし、座標情報とペアにして返します。
-	 * 
+	 *
 	 * @param target スクリーンショット撮影結果
 	 * @return マスク済の画像と座標情報のペア
 	 */
@@ -725,13 +729,61 @@ public class AssertionView extends TestWatcher {
 		return new ImageRectanglePair(image, target.getTarget().getRectangle().toRectangle());
 	}
 
+	/**
+	 * JSON出力用に小数点以下を丸める。target部分は指定した領域に収まる最大の短径となるようにする。
+	 *
+	 * @param target targetに指定した要素結果
+	 * @return JSON出力用のtargetのScreenAreaResult
+	 */
+	private ScreenAreaResult toTargetForJson(ScreenAreaResult target) {
+		ScreenArea screenArea = target.getScreenArea();
+		if (screenArea.getSelector() == null) {
+			return target;
+		}
+
+		RectangleArea area = target.getRectangle();
+		double x = Math.ceil(area.getX());
+		double y = Math.ceil(area.getY());
+		double height = Math.floor(area.getY() + area.getHeight()) - y;
+		double width = Math.floor(area.getX() + area.getWidth()) - x;
+		RectangleArea newArea = new RectangleArea(x, y, width, height);
+
+		return new ScreenAreaResult(target.getSelector(), newArea, screenArea);
+	}
+
+	/**
+	 * JSON出力用に小数点以下を丸める。exclude部分は指定した領域が収まる最小の短径となるようにする。
+	 *
+	 * @param excludes excludesに指定した要素結果のリスト
+	 * @return JSON出力用のexcludesのScreenAreaResultのリスト
+	 */
+	private List<ScreenAreaResult> toExcludesForJson(List<ScreenAreaResult> excludes) {
+		List<ScreenAreaResult> ret = new ArrayList<>();
+		for (ScreenAreaResult exclude : excludes) {
+			ScreenArea screenArea = exclude.getScreenArea();
+			if (screenArea.getSelector() == null) {
+				ret.add(exclude);
+			}
+
+			RectangleArea area = exclude.getRectangle();
+			double x = Math.floor(area.getX());
+			double y = Math.floor(area.getY());
+			double height = Math.ceil(area.getY() + area.getHeight()) - y;
+			double width = Math.ceil(area.getX() + area.getWidth()) - x;
+			RectangleArea newArea = new RectangleArea(x, y, width, height);
+
+			ret.add(new ScreenAreaResult(exclude.getSelector(), newArea, screenArea));
+		}
+		return ret;
+	}
+
 	//</editor-fold>
 
 	//<editor-fold desc="assertScreenshot">
 
 	/**
 	 * すでに取得したスクリーンショットについて、期待画像と一致するか検証します。一致しなければdiff画像を生成し、テストを失敗として中断します。
-	 * 
+	 *
 	 * @param screenshotResult {@link PtlWebDriver#takeScreenshot(String) takeScreenshot}を実行して取得した結果オブジェクト
 	 */
 	public void assertScreenshot(ScreenshotResult screenshotResult) {
@@ -740,7 +792,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * すでに取得したスクリーンショットについて、期待画像と一致するか検証します。一致しなければdiff画像を生成し、テストを失敗として中断します。
-	 * 
+	 *
 	 * @param message 失敗時に表示するメッセージ
 	 * @param screenshotResult {@link PtlWebDriver#takeScreenshot(String) takeScreenshot}を実行して取得した結果オブジェクト
 	 */
@@ -774,7 +826,7 @@ public class AssertionView extends TestWatcher {
 	 * すでに取得したスクリーンショットについて、期待画像と一致するか検証します。一致しなければdiff画像を生成し、テストを失敗とします。<br />
 	 * <br />
 	 * {@link #assertScreenshot(ScreenshotResult)}との違いは比較が失敗してもテストの実行を止めず、テストを最後まで実行します。
-	 * 
+	 *
 	 * @param screenshotResult {@link PtlWebDriver#takeScreenshot(String) takeScreenshot}を実行して取得した結果オブジェクト
 	 */
 	public void verifyScreenshot(ScreenshotResult screenshotResult) {
@@ -791,7 +843,7 @@ public class AssertionView extends TestWatcher {
 	 * すでに取得したスクリーンショットについて、期待画像と一致するか検証します。一致しなければdiff画像を生成し、テストを失敗とします。<br />
 	 * <br />
 	 * {@link #assertScreenshot(ScreenshotResult)}との違いは比較が失敗してもテストの実行を止めず、テストを最後まで実行します。
-	 * 
+	 *
 	 * @param message 失敗時に表示するメッセージ
 	 * @param screenshotResult {@link PtlWebDriver#takeScreenshot(String) takeScreenshot}を実行して取得した結果オブジェクト
 	 */
@@ -812,7 +864,7 @@ public class AssertionView extends TestWatcher {
 	/**
 	 * 画面全体のスクリーンショットを撮影し、指定の画像が現在のページ上に存在するかどうか検証します。<br />
 	 * ただし、テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の場合、検証は行われません。
-	 * 
+	 *
 	 * @param image 検証に使用する画像
 	 */
 	public void assertExist(BufferedImage image) {
@@ -822,7 +874,7 @@ public class AssertionView extends TestWatcher {
 	/**
 	 * 画面全体のスクリーンショットを撮影し、指定の画像が現在のページ上に存在するかどうか検証します。<br />
 	 * ただし、テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の場合、検証は行われません。
-	 * 
+	 *
 	 * @param message {@link AssertionError}を識別する文字列
 	 * @param image 検証に使用する画像
 	 */
@@ -853,7 +905,7 @@ public class AssertionView extends TestWatcher {
 	 * ただし、テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の場合、検証は行われません。<br />
 	 * <br />
 	 * {@link #assertExist(BufferedImage)}との違いは比較が失敗してもテストの実行を止めず、テストを最後まで実行します。
-	 * 
+	 *
 	 * @param image 検証に使用する画像
 	 */
 	public void verifyExists(BufferedImage image) {
@@ -871,7 +923,7 @@ public class AssertionView extends TestWatcher {
 	 * ただし、テスト実行モードが{@link com.htmlhifive.pitalium.core.config.ExecMode#SET_EXPECTED}の場合、検証は行われません。<br />
 	 * <br />
 	 * {@link #assertExist(BufferedImage)}との違いは比較が失敗してもテストの実行を止めず、テストを最後まで実行します。
-	 * 
+	 *
 	 * @param message {@link AssertionError}を識別する文字列
 	 * @param image 検証に使用する画像
 	 */
@@ -889,7 +941,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * 実行中のテストクラス名を取得します。
-	 * 
+	 *
 	 * @param description 実行中のテストのDescription
 	 * @return テストクラス名
 	 */
@@ -899,7 +951,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * 実行中のテストメソッド名を取得します。
-	 * 
+	 *
 	 * @param description 実行中のテストのDescription
 	 * @return テストメソッド名
 	 */
@@ -909,7 +961,7 @@ public class AssertionView extends TestWatcher {
 
 	/**
 	 * 配列をListに変換します。
-	 * 
+	 *
 	 * @param <T> 対象の配列の型
 	 * @param array 変換元配列
 	 * @return 変換後List
@@ -934,7 +986,7 @@ public class AssertionView extends TestWatcher {
 
 		/**
 		 * 画像と矩形情報のペアを持ったオブジェクトを生成します。
-		 * 
+		 *
 		 * @param image 画像
 		 * @param rectangle 矩形情報
 		 */
@@ -969,7 +1021,7 @@ public class AssertionView extends TestWatcher {
 		/**
 		 * 大きさが無い要素、存在しない要素を指定したセレクタ一覧を持った<br>
 		 * バリデーション結果オブジェクトを生成します。
-		 * 
+		 *
 		 * @param noAreaElementSelectors 大きさが無い要素を指定したセレクタ一覧
 		 * @param noElementSelectors 存在しない要素を指定したセレクタ一覧
 		 */
@@ -981,7 +1033,7 @@ public class AssertionView extends TestWatcher {
 
 		/**
 		 * バリデーションの結果がValidか否かを取得します。
-		 * 
+		 *
 		 * @return Validならtrue、Invalidならfalse
 		 */
 		public boolean isValid() {
